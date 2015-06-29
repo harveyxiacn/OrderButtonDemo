eb56e413dbf6526a48b938facec146f5a115b461
harveyxiacn <harvey1991cn@gmail.com>
2015-06-29 16:55:39 -0300

    Initial Commit

---
+++ OrderButtonDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OrderButtonDemo
+//
+//  Created by HarveyXia on 15/6/29.
+//  Copyright (c) 2015年 IDEA IT Solution Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
